9dd4dc6ff3096f18c853b520d14427fda0d29b99
Mei Albiol <meialbiol@gmail.com>
2018-06-21 21:48:46 +0200

    feat #6: refactorizado test

--- tests/CreateHtmlDataArrayTest.php
+++ tests/CreateHtmlDataArrayTest.php
@@ -7,18 +7,25 @@ use Calligraphy\CreateHtmlDataArray;
 
 class CreateHtmlDataArrayTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var CreateHtmlDataArray
+     */
+    protected $createData;
+
+    public function setUp()
+    {
+        $this->createData = new CreateHtmlDataArray();
+    }
 
     public function test_is_instance_of_CreateHtmlDataArray()
     {
-        $table =  new CreateHtmlDataArray();
-        $this->assertInstanceOf('Calligraphy\CreateHtmlDataArray', $table);
+        $this->assertInstanceOf('Calligraphy\CreateHtmlDataArray', $this->createData);
     }
 
     public function test_create_array_from_string()
     {
         $string = 'abcde';
-        $createSData = new CreateHtmlDataArray();
-        $data = $createSData->parse($string);
+        $data = $this->createData->parse($string);
         $this->assertEquals(5, count($data));
 
     }
@@ -26,8 +33,7 @@ class CreateHtmlDataArrayTest extends \PHPUnit\Framework\TestCase
     public function test_crear_associative_array_0_character_x_empty()
     {
         $string = 'ab';
-        $createSData = new CreateHtmlDataArray();
-        $data = $createSData->parse($string, 4);
+        $data = $this->createData->parse($string, 4);
         $this->assertEquals(4, count($data[0]));
         $this->assertEquals('', $data[0][1]);
 
@@ -37,8 +43,7 @@ class CreateHtmlDataArrayTest extends \PHPUnit\Framework\TestCase
     public function test_crear_associative_array_with_words()
     {
         $string = 'ab, cd';
-        $createSData = new CreateHtmlDataArray();
-        $data = $createSData->parse($string, 1, ',');
+        $data = $this->createData->parse($string, 1, ',');
         $this->assertEquals(2, count($data));
 
     }
@@ -46,8 +51,7 @@ class CreateHtmlDataArrayTest extends \PHPUnit\Framework\TestCase
     public function test_crear_associative_array_with_words_and_empty_columns()
     {
         $string = 'ab, cd, ef';
-        $createSData = new CreateHtmlDataArray();
-        $data = $createSData->parse($string, 4, ',');
+        $data = $this->createData->parse($string, 4, ',');
         $this->assertEquals(3, count($data));
         $this->assertEquals(4, count($data[0]));
 
